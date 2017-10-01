7ec4fecc173704f3ffda42daaaa031271a2cbbcc
Fernando <montesfer_98@hotmail.com>
2017-10-01 18:25:03 -0400

    New commitment trying to get environment variables to display

--- groupme_messages.py
+++ groupme_messages.py
@@ -4,37 +4,6 @@ Created on Tue Sep 26 01:14:45 2017
 
 @author: Fernando
 """
-
-import requests
-#import json #not necessary apparently
-
-#Attempt to download GroupMe messages
-
-base_url = "https://api.groupme.com/v3"
-token = "?token=JWMisdf6K8a2756JNSHnax0ovSHtlCU7eQbaIs4X"
-
-#Get groups
-groups_index = base_url+"/groups"+token
-
-
-#make a get request for groups
-response = requests.get(groups_index)
-print(response.status_code)
-#print(response.content[0:100]) #prints out the first hundred characters
-print(response.json().keys())
-print(response.json()['meta'])
-print(response.encoding)
-print(response.json()['response'][0]['id'])
-print(response.json()['response'][0]['name'])
-print(response.json()['response'][1]['id'])
-print(response.json()['response'][0])
-
-def clean_json(json_response):
-    print("\n"+json_response['response'][0]['name'])
-    
-if __name__=="__main__":
-    clean_json(response.json())
-
 #Fascinating... I will have to continue messing around with this, but let's use this to establish some of
 #the things I want this app to be able to do:
     #1 - Pull all data from response/json but only from the first group and clean
@@ -47,3 +16,74 @@ if __name__=="__main__":
     #7 - Store different messages from different groupchats in different files (text or csv? probably csv)
     #8 - download_messages(gc_name, nmbr_prev_msgs), upload_messages_server(msg_file(text/csv)), clean_messages(msg_file(text/csv)) 
     #8.1 - the above are the minimum that should be considered
+    
+import requests
+import os
+
+#import json #not necessary apparently
+
+#Attempt to download GroupMe messages
+
+base_url = "https://api.groupme.com/v3"
+token = os.getenv('TOKEN')
+
+#Get groups
+print(token)
+#groups_index = base_url+"/groups"+token
+
+
+#make a get request for groups
+response = requests.get(groups_index)
+#print(response.status_code)
+##print(response.content[0:100]) #prints out the first hundred characters
+#print(response.json().keys())
+#print(response.json()['meta'])
+#print(response.encoding)
+#print(response.json()['response'][0]['id'])
+#print(response.json()['response'][0]['name'])
+#print(response.json()['response'][1]['id'])
+#print(response.json()['response'][0])       
+
+#def flatten_json(json_response, delim):
+#    #print("\n"+json_response['response'][0]['name'])
+#    val = {}
+#    for i in json_response.keys():
+#        if isinstance(json_response[i], dict):
+#            get = flatten_json(json_response[i], delim)
+#            for j in get.keys():
+#                val[i + delim + j ] = get[j]
+#        else:
+#            val[i] = json_response[i]
+#
+#    return val
+
+def convert_json(json_response):
+    #print("\n",json_response['response'][0]['name'])
+    #print(json_response.keys())
+    #val = {}
+    if isinstance(json_response, dict):
+        for key in json_response.keys():
+            #print(json_response[key],'\n')
+            if isinstance(json_response[key], dict):
+#                #nested_dict = convert_json(json_response[key])
+#                for new_key,value in value.items():
+#                    #print(new_key, ' ', value)
+#                    print('Hi')
+                nested_dict = convert_json(json_response[key])
+                convert_json(nested_dict)
+#            elif isinstance(json_response[key], list):
+#                json_response[key]
+            else:
+#                #print('\n',json_response[key])
+                #print(value,'\n'+'\n')
+                print(json_response.values())
+                return json_response.values()
+                #pass
+    else:
+        #return json_response.values()
+        print(json_response.values())
+        #pass
+    
+if __name__=="__main__":
+    #print(flatten_json(response.json(),' '))
+    convert_json(response.json())
